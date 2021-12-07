81737bb108259ed4ed451631ebe9b599b2bb0576
tina-chan <kassi_0123@kassi-hp.info>
2021-12-08 00:01:30 +0900

    コマンド実装

--- src/main/java/xyz/n7mn/dev/playerlistsync/PlayerListSync.java
+++ src/main/java/xyz/n7mn/dev/playerlistsync/PlayerListSync.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.plugin.Plugin;
+import xyz.n7mn.dev.playerlistsync.command.ServerPlayerList;
 import xyz.n7mn.dev.playerlistsync.config.ConfigJson;
 
 import java.io.*;
@@ -19,6 +20,7 @@ public final class PlayerListSync extends Plugin {
     private Connection con = null;
     private Timer timer = new Timer();
     private int playerCount = 0;
+    private ConfigJson configJson;
 
     @Override
     public void onEnable() {
@@ -46,7 +48,7 @@ public final class PlayerListSync extends Plugin {
 
 
         getProxy().getPluginManager().registerListener(this, new WaterEventListener(this));
-
+        getProxy().getPluginManager().registerCommand(this, new ServerPlayerList(this));
 
         // 設定ファイル読み込んで定期実行タスク生成
         new Thread(()->{
@@ -62,21 +64,14 @@ public final class PlayerListSync extends Plugin {
             } catch (IOException ex){
                 ex.printStackTrace();
             }
-            ConfigJson config1 = new Gson().fromJson(sb.toString(), ConfigJson.class);
-
-            try {
-                con = DriverManager.getConnection("jdbc:mysql://" + config1.getMySQLServer() + ":" + config1.getMySQLPort() + "/" + config1.getMySQLDatabase() + config1.getMySQLOption(), config1.getMySQLUsername(), config1.getMySQLPassword());
-                con.setAutoCommit(true);
-            } catch (SQLException e){
-                e.printStackTrace();
-            }
+            configJson = new Gson().fromJson(sb.toString(), ConfigJson.class);
 
             TimerTask task = new TimerTask() {
                 @Override
                 public void run() {
                     if (con == null){
                         try {
-                            con = DriverManager.getConnection("jdbc:mysql://" + config1.getMySQLServer() + ":" + config1.getMySQLPort() + "/" + config1.getMySQLDatabase() + config1.getMySQLOption(), config1.getMySQLUsername(), config1.getMySQLPassword());
+                            con = DriverManager.getConnection("jdbc:mysql://" + configJson.getMySQLServer() + ":" + configJson.getMySQLPort() + "/" + configJson.getMySQLDatabase() + configJson.getMySQLOption(),configJson.getMySQLUsername(), configJson.getMySQLPassword());
                             con.setAutoCommit(true);
                         } catch (SQLException e){
                             e.printStackTrace();
@@ -89,14 +84,14 @@ public final class PlayerListSync extends Plugin {
                                 PreparedStatement statement = con.prepareStatement("SELECT * FROM ServerList");
                                 statement.execute();
                                 statement.close();
-                            } catch (SQLException ex1){
-                                con = DriverManager.getConnection("jdbc:mysql://" + config1.getMySQLServer() + ":" + config1.getMySQLPort() + "/" + config1.getMySQLDatabase() + config1.getMySQLOption(), config1.getMySQLUsername(), config1.getMySQLPassword());
+                            } catch (Exception ex1){
+                                con = DriverManager.getConnection("jdbc:mysql://" + configJson.getMySQLServer() + ":" + configJson.getMySQLPort() + "/" + configJson.getMySQLDatabase() + configJson.getMySQLOption(), configJson.getMySQLUsername(), configJson.getMySQLPassword());
                                 con.setAutoCommit(true);
                             }
 
                             PreparedStatement statement1 = con.prepareStatement("SELECT * FROM ServerList WHERE ServerName = ? AND ServerNo = ?");
-                            statement1.setString(1, config1.getServerName());
-                            statement1.setInt(2, config1.getServerNo());
+                            statement1.setString(1, configJson.getServerName());
+                            statement1.setInt(2, configJson.getServerNo());
                             ResultSet set = statement1.executeQuery();
 
                             boolean isFound = false;
@@ -116,13 +111,13 @@ public final class PlayerListSync extends Plugin {
                             if (isFound){
                                 statement2 = con.prepareStatement("UPDATE `ServerList` SET `PlayerCount`= ? WHERE ServerName = ? AND ServerNo = ?");
                                 statement2.setInt(1, playerCount);
-                                statement2.setString(2, config1.getServerName());
-                                statement2.setInt(3, config1.getServerNo());
+                                statement2.setString(2, configJson.getServerName());
+                                statement2.setInt(3, configJson.getServerNo());
                             } else {
                                 statement2 = con.prepareStatement("INSERT INTO `ServerList`(`UUID`, `ServerName`, `ServerNo`, `PlayerCount`) VALUES (?,?,?,?)");
                                 statement2.setString(1, UUID.randomUUID().toString());
-                                statement2.setString(2, config1.getServerName());
-                                statement2.setInt(3, config1.getServerNo());
+                                statement2.setString(2, configJson.getServerName());
+                                statement2.setInt(3, configJson.getServerNo());
                                 statement2.setInt(4, playerCount);
                             }
 
@@ -131,7 +126,7 @@ public final class PlayerListSync extends Plugin {
 
                             tempPlayerCount = 0;
                             PreparedStatement statement3 = con.prepareStatement("SELECT * FROM ServerList WHERE ServerName = ?");
-                            statement3.setString(1, config1.getServerName());
+                            statement3.setString(1, configJson.getServerName());
                             ResultSet set1 = statement3.executeQuery();
                             while (set1.next()){
                                 tempPlayerCount = tempPlayerCount + set1.getInt("PlayerCount");
@@ -142,6 +137,7 @@ public final class PlayerListSync extends Plugin {
 
                         } catch (SQLException ex){
                             ex.printStackTrace();
+                            timer.cancel();
                         }
                     }).start();
                 }
@@ -169,5 +165,13 @@ public final class PlayerListSync extends Plugin {
 
     public int getPlayerCount() {
         return playerCount;
+    }
+
+    public Connection getConnect() {
+        return con;
+    }
+
+    public ConfigJson getConfig() {
+        return configJson;
     }
 }
---
+++ src/main/java/xyz/n7mn/dev/playerlistsync/command/ServerPlayerList.java
@@ -0,0 +1,65 @@
+package xyz.n7mn.dev.playerlistsync.command;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.plugin.Command;
+import xyz.n7mn.dev.playerlistsync.PlayerListSync;
+import xyz.n7mn.dev.playerlistsync.config.ConfigJson;
+
+import java.sql.*;
+import java.util.Map;
+import java.util.UUID;
+
+public class ServerPlayerList extends Command {
+
+    private final PlayerListSync plugin;
+
+    public ServerPlayerList(PlayerListSync plugin){
+        super("wlist");
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+
+        ConfigJson config = plugin.getConfig();
+
+        if (!sender.hasPermission("playersync.op")){
+            return;
+        }
+
+        new Thread(()->{
+            Connection con = plugin.getConnect();
+
+            try {
+                try {
+                    PreparedStatement statement = con.prepareStatement("SELECT * FROM ServerList");
+                    statement.execute();
+                    statement.close();
+                } catch (Exception ex1){
+                    con = DriverManager.getConnection("jdbc:mysql://" + config.getMySQLServer() + ":" + config.getMySQLPort() + "/" + config.getMySQLDatabase() + config.getMySQLOption(), config.getMySQLUsername(), config.getMySQLPassword());
+                    con.setAutoCommit(true);
+                }
+
+                PreparedStatement statement1 = con.prepareStatement("SELECT * FROM ServerList, ServerNameList WHERE ServerList.UUID = ServerNameList.UUID AND ServerList.ServerName = ? ORDER BY ServerList.ServerName ASC, ServerList.ServerNo ASC");
+                statement1.setString(1, config.getServerName());
+                int tempCount = 0;
+                ResultSet set = statement1.executeQuery();
+                while (set.next()){
+                    tempCount = tempCount + set.getInt("PlayerCount");
+                    sender.sendMessage(new TextComponent(ChatColor.GREEN+"["+set.getString("ServerNameList.ServerName")+"] "+ChatColor.YELLOW+"("+set.getInt("PlayerCount")+")"+ChatColor.RESET));
+                }
+                sender.sendMessage(new TextComponent("Total players online: "+tempCount));
+                set.close();
+                statement1.close();
+
+            } catch (SQLException ex){
+                ex.printStackTrace();
+            }
+
+        }).start();
+
+    }
+}
